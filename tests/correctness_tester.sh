e5cf2e170b3146dcf452f198af5d4f4721d37f4d
reivvax <gracjanjr.barski@gmail.com>
2025-01-03 12:14:27 +0100

    Correctness tester ready

--- tests/correctness_tester.sh
+++ tests/correctness_tester.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -x
 
 REFERENCE=$(realpath "../build/reference/reference")
 NONRECURSIVE=$(realpath "../build/nonrecursive/nonrecursive")
@@ -11,10 +12,10 @@ OUTPUT_DIR="./correctness_output"
 
 GREEN="\033[0;32m"
 RED="\033[0;31m"
+YELLOW="\033[0;33m"
 NC="\033[0m"
 
-echo -e "===Running tests ${RED}without valgrind${NC}==="
-
+echo -e "===Running tests ${YELLOW}without valgrind${NC}==="
 counter=1
 
 limit="prlimit --as=$(($THREADS_FOR_PARALLEL*128*1024*1024)) time -f'%e seconds, %M kb (max RSS),  exit=%x' timeout --foreground 10s"
@@ -22,18 +23,28 @@ limit="prlimit --as=$(($THREADS_FOR_PARALLEL*128*1024*1024)) time -f'%e seconds,
 for test_file in "$TEST_DIR"/test_*.in; do
     
     "$REFERENCE" < $test_file | head -n 1 > "$OUTPUT_DIR/reference.out"
-    (eval $limit "$NONRECURSIVE" < "$test_file" | head -n 1 > $OUTPUT_DIR/nonrecursive.out) 2> "$OUTPUT_DIR/nonrecursive.log"
-    (eval $limit "$PARALLEL" < "$test_file" | head -n 1 > $OUTPUT_DIR/parallel.out) 2> "$OUTPUT_DIR/parallel.log"
+    "$NONRECURSIVE" < "$test_file" | head -n 1 > $OUTPUT_DIR/nonrecursive.out
+    eval $limit "$PARALLEL" < "$test_file" > $OUTPUT_DIR/parallel.out 2> "$OUTPUT_DIR/parallel.log"
+
+    if [ $? -ne 0 ] ; then
+        echo -e "Test $counter: \t${RED}FAIL${NC}"
+        echo "Parallel failed, details in $OUTPUT_DIR/parallel.log"
+        exit 1
+    fi
+
+    # Truncate the file
+    head -n 1 $OUTPUT_DIR/parallel.out > $OUTPUT_DIR/parallel.tmp && mv $OUTPUT_DIR/parallel.tmp $OUTPUT_DIR/parallel.out
+
 
     if diff -q $OUTPUT_DIR/reference.out $OUTPUT_DIR/nonrecursive.out > /dev/null && diff -q $OUTPUT_DIR/reference.out $OUTPUT_DIR/parallel.out > /dev/null; then
         echo -e "Test $counter: \t${GREEN}PASS${NC}"
     else
         echo -e "Test $counter: \t${RED}FAIL${NC}"
         if ! diff $OUTPUT_DIR/reference.out $OUTPUT_DIR/nonrecursive.out > $OUTPUT_DIR/nonrecursive.diff; then
-            echo "NONRECURSIVE output differs"
+            echo "NONRECURSIVE output differs, details in $OUTPUT_DIR/nonrecursive.diff"
         fi
         if ! diff $OUTPUT_DIR/reference.out $OUTPUT_DIR/parallel.out > $OUTPUT_DIR/parallel.diff; then
-            echo "PARALLEL output differs"
+            echo "PARALLEL output differs, details in $OUTPUT_DIR/parallel.diff, log in $OUTPUT_DIR/parallel.log"
         fi
         exit 1
     fi
@@ -41,7 +52,7 @@ for test_file in "$TEST_DIR"/test_*.in; do
     counter=$((counter+1))
 done
 
-echo -e "===Running tests ${RED}with valgrind${NC}==="
+echo -e "===Running tests ${YELLOW}with valgrind${NC}==="
 
 counter=1
 
@@ -51,7 +62,7 @@ for test_file in "$TEST_DIR"/test_*.in; do
 
     if [ $? -ne 0 ] ; then
         echo -e "Test $counter: \t${RED}FAIL${NC}"
-        echo -e "Nonrecursive failed valgrind (details in $OUTPUT_DIR/nonrecursive.val)"
+        echo -e "Nonrecursive failed valgrind, details in $OUTPUT_DIR/nonrecursive.val"
         exit 1
     fi
     
@@ -59,7 +70,7 @@ for test_file in "$TEST_DIR"/test_*.in; do
 
     if [ $? -ne 0 ] ; then
         echo -e "Test $counter: \t${RED}FAIL${NC}"
-        echo -e "Parallel failed valgrind (details in $OUTPUT_DIR/parallel.val)"
+        echo -e "Parallel failed valgrind, details in $OUTPUT_DIR/parallel.val"
         exit 1
     fi
 
--- tests/time_tester.sh
+++ tests/time_tester.sh
@@ -1,40 +1,107 @@
 #!/bin/bash
-
-REFERENCE="./build/reference/reference"
-NONRECURSIVE="./build/nonrecursive/nonrecursive"
-PARALLEL="./build/parallel/parallel"
+set -x
+REFERENCE="../build/reference/reference"
+NONRECURSIVE="../build/nonrecursive/nonrecursive"
+PARALLEL="../build/parallel/parallel"
 
 TEST_DIR="./tests"
+OUTPUT_DIR="./time_output"
 
+BOLD="\033[1m"
 GREEN="\033[0;32m"
 RED="\033[0;31m"
+YELLOW="\033[0;33m"
 NC="\033[0m"
 
-T_VALUES=(1 2 4 8 16 32 64)
-D_VALUES=(5 10 15 20 25 30 32 34)
+OUTPUT_TO_FILE=false
+BRIEF_MODE=false
+RUN_REFERENCE=false
+T_LIMIT=5
 
-counter=1
+# Args
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -f|--file)
+            OUTPUT_TO_FILE=true
+            shift
+            ;;
+        -b|--brief)
+            BRIEF_MODE=true
+            shift
+            ;;
+        -r|--reference)
+            RUN_REFERENCE=true
+            shift
+            ;;
+        -n|--limit)
+            if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+                LIMIT="$2"
+                shift 2
+            else
+                echo "Error: -n|--limit requires a positive integer argument."
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Error: Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
 
-for test_file in "$TEST_DIR"/test_*.in; do
-    
-    "$REFERENCE" < $test_file > reference.out
-    "$NONRECURSIVE" < $test_file > nonrecursive.out
-    "$PARALLEL" < $test_file > parallel.out
+if (( T_LIMIT > 6 )); then
+    echo -e "${YELLOW}Warning: Running with limit = 6${NC}"
+    T_LIMIT=6
+fi
 
-    if diff -q <(head -n 1 reference.out) <(head -n 1 nonrecursive.out) > /dev/null && diff -q <(head -n 1 reference.out) <(head -n 1 parallel.out) > /dev/null; then
-        echo -e "Test $counter: \t${GREEN}PASS${NC}"
-    else
-        echo -e "Test $counter: \t${RED}FAIL${NC}"
-        if ! diff <(head -n 1 reference.out) <(head -n 1 nonrecursive.out) > nonrecursive.diff; then
-            echo "NONRECURSIVE output differs"
+# D_VALUES=(5 10 15 20 25 30 32 34)
+D_VALUES=(5 10 15 20)
+
+
+current_t=0
+
+for d in $D_VALUES; do # Iteration over d parameter
+
+    while true; do # Iteration over t parameter
+
+        if (( current_t > T_LIMIT )); then
+            break
         fi
-        if ! diff <(head -n 1 reference.out) <(head -n 1 parallel.out) > parallel.diff; then
-            echo "PARALLEL output differs"
-        fi
-        exit 1
-    fi
 
-    counter=$((counter+1))
+        THREADS_COUNT=$(( 2 ** current_t ))
+
+        if [ "$BRIEF_MODE" = "true" ]; then
+            echo -e "${BOLD}NOT IMPLEMENTED${NC}"
+        else
+            
+            if [ "$RUN_REFERENCE" = "true" ]; then
+                echo -e "${BOLD}NOT IMPLEMENTED${NC}"
+            else
+                # here run the program witch such command:
+                # time -f'<SOME FSTRING TO GET REAL TIME IN SECONDS> echo 1 $d 0 1 1 | $REFERENCE > (SOME VARIABLE)
+                reference_output=$( (time -f\'%e\' echo "1 $d 0 1 1" | $REFERENCE) 2>&1 )
+                reference_real_time=$(echo "$reference_output" | awk '{print $1}')
+
+                # time -f'<SOME FSTRING TO GET REAL TIME AND USER TIME IN SECONDS> echo $THREADS_COUNT $d 0 1 1 | $PARALLEL > (SOME VARIABLE)
+                parallel_output=$( { time echo "$THREADS_COUNT $d 0 1 1" | $PARALLEL; } 2>&1 )
+                parallel_real_time=$(echo "$parallel_output" | grep real | awk '{print $2}')                
+                parallel_user_time=$(echo "$parallel_output" | grep user | awk '{print $2}')
+                
+                cpu_utilization=$(echo "$parallel_user_time / ($parallel_real_time * $THREADS_COUNT)" | bc -l)
+
+                parallelization_factor=$(echo "$reference_real_time / $parallel_real_time" | bc -l)
+
+
+                echo -e "For d=$d and THREADS_COUNT=$THREADS_COUNT:"
+                echo -e "  Reference Real Time: $reference_real_time seconds"
+                echo -e "  Parallel Real Time: $parallel_real_time seconds"
+                echo -e "  CPU Utilization: $cpu_utilization"
+                echo -e "  Parallelization Factor: $parallelization_factor"
+            fi
+        fi
+
+        current_t=$((current_t+1))
+    done
 done
 
 rm *.out
